62cba10198009f42a9aa2f22b208f311b730685b
arin <yurins0103@naver.com>
2024-09-26 01:53:08 +0900

    data import, export

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ import Projects from './components/Projects';
 import Header from './components/Header';
 
 function App() {
+
   const [theme, setTheme] = useState(false);
 
   const changeMode = () => {
     setTheme(!theme);
   };
 
+  function totop () {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div className="App" id={theme ? 'dark-mode' : 'light-mode'}>
+
       {/* header */}
       <Header theme={theme} changeMode={changeMode} />
 
@@ -26,7 +32,7 @@ function App() {
       <Introduction theme={theme} setTheme={setTheme} changeMode={changeMode}/>
 
       {/* projects */}
-      <Projects />
+      <Projects  />
 
       {/* board */}
       <div className="container-md mt-4">
@@ -34,6 +40,11 @@ function App() {
       </div>
 
       {/* footer */}
+      <div className='footer'>
+        footer가 과연 필요한가?
+      </div>
+
+      <img src='/image/totop.png' alt='totop' className='totop' onClick={totop}/>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,107 @@
+{/* react */}
+const react = [
+  {
+    id: 1,
+    title: '[개인] Apple Store',
+    subTitle: 'React / 반응형 / JWT / Route / 기능추가중.. ',
+    mainFunction: '핵심기능 : JWT로 로그인화면 구현 및 Route활용, 오픈캔버스',
+    result: '대규모 프로젝트의 경우 코드의 정형화, 재사용성 고려, 계획적인 기능구현의 중요성을 깨닫게 되었어요',
+    link: '/appleStore',
+    video: 'image/react_applestore.mp4'
+  },
+  {
+    id: 2,
+    title: '[개인] Shoes Mall',
+    subTitle: 'React / axios',
+    mainFunction: '핵심기능 : 더보기 버튼 클릭시 서버에 데이터가 있으면 가져오고 없으면 그 버튼으로 전환하여 더이상 클릭되지 않도록 구현',
+    result: '데이터를 가져올 때는 한번에 다 가져오는 방법과 요청할때마다 가져오는 방법이 있고 메모리사용의 차이가 있다는걸 알게 되었어요',
+    link: '/myshop',
+    video: 'image/react_shoesmall.mp4'
+  },
+  {
+    id: 3,
+    title: '[개인] Todo List App',
+    subTitle: 'React / crud',
+    mainFunction: '핵심기능 : input으로 내용추가, 및 수정기능과 삭제버튼 기능구현',
+    result: 'fetch로 요청해서 가져온 데이터를 컨트롤하는 방법과 axios의 편리성을 깨닫게 되었어요',
+    link: '/myTodoList',
+    video: 'image/react_todoapp.mp4'
+  },
+  {
+    id: 4,
+    title: '[팀] Shopping Mall',
+    subTitle: 'React / API/ JWT',
+    mainFunction: '핵심기능 : paymentAPI, 로그인JWT 기능',
+    result: '서버와 클라이언트간의 호출법과 통신규제에 대해 알게되었어요, 특히 axios delete메서드는 다른 요청들과 달리 {data : body} 와 같이 key:value로 보내야 하는걸 알게되었어요',
+    link: '/myTodoList',
+    video: 'image/react_shoppingmall.png'
+  },
+]
+
+{/* vue */}
+const vue = [
+  {
+    id: 1,
+    title: 'Apple Store',
+    subTitle: 'React / 반응형 / JWT / Route / 기능추가중.. ',
+    mainFunction: '핵심기능 : JWT로 로그인화면 구현 및 Route활용, 오픈캔버스',
+    result: '대규모 프로젝트의 경우 코드의 정형화, 재사용성 고려, 계획적인 기능구현의 중요성을 깨닫게 되었어요',
+    link: '/appleStore',
+    video: 'image/react_applestore.mp4'
+  },
+  {
+    id: 2,
+    title: 'Shoes Mall',
+    subTitle: 'React / axios',
+    mainFunction: '핵심기능 : 더보기 버튼 클릭시 서버에 데이터가 있으면 가져오고 없으면 그 버튼으로 전환하여 더이상 클릭되지 않도록 구현',
+    result: 'fetch로 요청해서 가져온 데이터를 컨트롤하는 방법을 깨닫게 되었어요',
+    link: '/myshop',
+    video: 'image/react_shoesmall.mp4'
+  }
+]
+
+{/* javascript */}
+const javascript = [
+  {
+    id: 1,
+    title: 'Photo Matching Game',
+    subTitle: '[개인] Javascript',
+    mainFunction: '핵심기능 : 배열에 이미지를 담아 저장하고 클릭한 두개의 상태가 같지 않을 경우 다시 닫히고 맞추면 이미지가 보여지는 기능 구현',
+    result: '배열을 다루며 관련 메서드에대한 공부가 많이 되었어요',
+    link: '/mini_photoMatching',
+    video: 'image/javascript_picturematchinggame.mp4'
+  },
+  {
+    id: 2,
+    title: 'Typing Words Game',
+    subTitle: '[개인] Javascript',
+    mainFunction: '핵심기능 : 10초 안에 랜덤한 단어를 보여주고 타이핑을 알맞게 친 갯수를 저장하여 보여지도록 구현',
+    result: '랜덤함수를 사용하여 단어를 만드는법, 검증의 중요성을 느꼈어요',
+    link: '/mini_typingWords',
+    video: 'image/javascript_typingwords.mp4'
+  },
+  {
+    id: 3,
+    title: 'Hangman Game',
+    subTitle: '[개인] Javascript',
+    mainFunction: '핵심기능 : setWord를 설정하면 그 단어를 알파벳 버튼을 눌러 6번 안에 맞추어야 하는 게임으로 맞는 것을 클릭하면 클릭한 알파벳이 보여지고 못맞춘 것은 보여지지 않도록 구현',
+    result: '버튼을 클릭했을 때, 남은횟수와 연관하여 생각해서 기능구현하면서 각 기능들의 연관성과 밀접성을 파악하여 적절히 구현해야 하는걸 느꼈어요',
+    link: '/myHangmanGame',
+    video: 'image/javascript_hangmangame.mp4'
+  },
+]
+
+{/* java */}
+const java = [
+  {
+    id: 1,
+    title: 'Animal Hospital Reservation',
+    subTitle: '[Team] Java Project',
+    mainFunction: '핵심기능 : 날짜예약시스템과 커뮤니티 기능등을 구현',
+    result: '담당 : Q&A page : 1. 글 클릭시 상세보기 이동 2.비밀번호로 인증후 수정페이지 이동기능 구현',
+    link: '/mini_photoMatching',
+    video: 'image/java_animalhospitalreservation.png'
+  },
+]
+
+export {react, vue, javascript, java}
